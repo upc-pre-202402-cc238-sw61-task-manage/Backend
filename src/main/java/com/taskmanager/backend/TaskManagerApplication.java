31cb81a314177fe749113d5f1c7c24e29032fa36
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-09-10 19:18:11 -0500

    fix: enabled the JDP auditing

--- src/main/java/com/taskmanager/backend/TaskManagerApplication.java
+++ src/main/java/com/taskmanager/backend/TaskManagerApplication.java
@@ -2,8 +2,11 @@ package com.taskmanager.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
