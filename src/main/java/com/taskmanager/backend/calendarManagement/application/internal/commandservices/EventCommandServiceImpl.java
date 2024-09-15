a4cc2f30a2f39b79d5051ed5a0ef379b4e901a40
PieroM1 <105945018+PieroM1@users.noreply.github.com>
2024-09-14 19:52:34 -0500

    feat(events): added command and query services implementation

---
+++ src/main/java/com/taskmanager/backend/calendarManagement/application/internal/commandservices/EventCommandServiceImpl.java
@@ -0,0 +1,71 @@
+package com.taskmanager.backend.calendarManagement.application.internal.commandservices;
+
+
+import com.taskmanager.backend.calendarManagement.domain.model.aggregates.Event;
+import com.taskmanager.backend.calendarManagement.domain.model.commands.CreateEventCommand;
+import com.taskmanager.backend.calendarManagement.domain.model.commands.DeleteEventCommand;
+import com.taskmanager.backend.calendarManagement.domain.model.commands.UpdateEventCommand;
+import com.taskmanager.backend.calendarManagement.domain.model.valueobjects.Project;
+import com.taskmanager.backend.calendarManagement.domain.services.EventCommandService;
+import com.taskmanager.backend.calendarManagement.infrastructure.persistence.jpa.repositories.EventRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EventCommandServiceImpl implements EventCommandService {
+    private final EventRepository eventRepository;
+
+    public EventCommandServiceImpl(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+
+    @Override
+    public Long handle(CreateEventCommand command) {
+        var projectId = new Project(command.project());
+        if(eventRepository.existsByTitleAndProject(command.title(), projectId)){
+            throw new IllegalArgumentException("Event title already exists");
+        }
+        var event = new Event(command);
+        try{
+            eventRepository.save(event);
+        }catch(Exception e){
+            throw new IllegalArgumentException("Error while creating event" + e.getMessage());
+        }
+        return event.getId();
+    }
+
+    @Override
+    public Optional<Event> handle(UpdateEventCommand command) {
+        if(eventRepository.existsByTitleAndIdIsNot(command.title(), command.id())){
+            throw new IllegalArgumentException("Event with same title already exists");
+        }
+        var result = eventRepository.findById(command.id());
+        if(result.isEmpty()){
+            throw new IllegalArgumentException("Event does not exist");
+        }
+        var eventToUpdate = result.get();
+        try{
+            var updatedEvent = eventRepository.save(eventToUpdate.updateInformation(command.title(), command.description(), command.day(), command.month(), command.year()));
+            return Optional.of(updatedEvent);
+        } catch (Exception e){
+            throw new IllegalArgumentException("Error while updating event" + e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void handle(DeleteEventCommand command) {
+        if(!eventRepository.existsById(command.eventId())){
+            throw new IllegalArgumentException("Event does not exist");
+        }
+        try{
+            eventRepository.existsById(command.eventId());
+        }catch (Exception e){
+            throw new IllegalArgumentException("Error while deleting event" + e.getMessage());
+        }
+
+    }
+}
--- src/main/java/com/taskmanager/backend/calendarManagement/domain/services/EventQueryService.java
+++ src/main/java/com/taskmanager/backend/calendarManagement/domain/services/EventQueryService.java
@@ -1,18 +1,18 @@
 package com.taskmanager.backend.calendarManagement.domain.services;
 
+import com.taskmanager.backend.calendarManagement.domain.model.aggregates.Event;
 import com.taskmanager.backend.calendarManagement.domain.model.queries.GetAllEventsByUserIdQuery;
 import com.taskmanager.backend.calendarManagement.domain.model.queries.GetAllEventsByProjectIdQuery;
 import com.taskmanager.backend.calendarManagement.domain.model.queries.GetEventByIdQuery;
-import org.springframework.scheduling.config.Task;
 
 import java.util.Optional;
 import java.util.List;
 
 public interface EventQueryService {
 
-    Optional<Task> handle(GetEventByIdQuery query);
-    List<Task> handle(GetAllEventsByProjectIdQuery query);
-    List<Task> handle(GetAllEventsByUserIdQuery query);
+    Optional<Event> handle(GetEventByIdQuery query);
+    List<Event> handle(GetAllEventsByProjectIdQuery query);
+    List<Event> handle(GetAllEventsByUserIdQuery query);
 
 
 }
