43c330110a0179a44f5eb1a4033ee4129cf7cf98
PieroM1 <105945018+PieroM1@users.noreply.github.com>
2024-09-14 21:28:29 -0500

    feat(events): added resources and assemblers for interfaces layer

---
+++ src/main/java/com/taskmanager/backend/calendarManagement/interfaces/transform/UpdateEventCommandFromResourceAssembler.java
@@ -0,0 +1,11 @@
+package com.taskmanager.backend.calendarManagement.interfaces.transform;
+
+import com.taskmanager.backend.calendarManagement.domain.model.commands.UpdateEventCommand;
+import com.taskmanager.backend.calendarManagement.interfaces.resources.UpdateEventResource;
+
+public class UpdateEventCommandFromResourceAssembler {
+
+    public static UpdateEventCommand toCommandFromResource(Long eventId, UpdateEventResource resource) {
+        return new UpdateEventCommand(eventId, resource.title(), resource.description(), resource.day(), resource.month(), resource.year());
+    }
+}
