e603d0d4be3e397b01565a8c1dab2830b930c1e1
PieroPq <pieropericheq@gmail.com>
2024-11-19 03:23:52 -0500

    feat: try to fix bug email

--- src/main/java/com/taskmanager/backend/profiles/domain/model/commands/CreateProfileCommand.java
+++ src/main/java/com/taskmanager/backend/profiles/domain/model/commands/CreateProfileCommand.java
@@ -1,4 +1,4 @@
 package com.taskmanager.backend.profiles.domain.model.commands;
 
-public record CreateProfileCommand (String firstName, String lastName, String email, String phoneNumber, Long userId) {
+public record CreateProfileCommand(String firstName, String lastName, String phoneNumber, String email, Long userId) {
 }
